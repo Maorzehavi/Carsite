44207678e86223ecbff03082509a4d057bb60283
Maorz <maorzehavi@outlook.com>
2024-07-21 13:21:39 +0300

    creating RetryPolicy

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,5 +1,8 @@
+using System.Net;
 using MongoDB.Driver;
 using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
 using SearchService.Models;
 using SearchService.Services;
@@ -9,20 +12,34 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionSvcHttpClient>();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>()
+    .AddPolicyHandler(GetRetryPolicy());
 
 var app = builder.Build();
 
 app.UseAuthorization();
 
 app.MapControllers();
-try
+
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    DbInitalizer.InitDb(app).Wait();
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
+    try
+    {
+        await DbInitalizer.InitDb(app);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+
+});
+
+
 
 app.Run();
+
+static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
